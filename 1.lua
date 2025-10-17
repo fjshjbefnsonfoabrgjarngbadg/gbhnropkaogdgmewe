dae7f3509127545c71e8082eb0a4e90e6b47c684
fjshjbefnsonfoabrgjarngbadg <carshub123@gmail.com>
2025-10-17 05:48:40 -0300

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -200,7 +200,7 @@ local SectionChildHeight = MenuSize.y - (2 * SectionsPadding)
 local ColumnWidth = (SectionChildWidth - (SectionsPadding * 3)) / 2
 local HalfHeight = (SectionChildHeight - (SectionsPadding * 3)) / 2
 
-local MenuWindow = MachoMenuTabbedWindow("Beta V1.4", MenuStartCoords.x, MenuStartCoords.y, MenuSize.x, MenuSize.y, TabsBarWidth)
+local MenuWindow = MachoMenuTabbedWindow("AN4 :)", MenuStartCoords.x, MenuStartCoords.y, MenuSize.x, MenuSize.y, TabsBarWidth)
 MachoMenuSetKeybind(MenuWindow, 0x14)
 MachoMenuSetAccent(MenuWindow, 75, 0, 130)
 
